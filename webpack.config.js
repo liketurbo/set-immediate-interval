4c69e44e2779f07b27ccbb3d784bc4ad0ac42a9c
Ramzan Chasygov <theliketurbo@gmail.com>
2019-06-04 19:45:43 +0300

    added test coverage

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const commonConfig = {
     index: path.join(__dirname, 'src', 'index.ts')
   },
   output: {
-    filename: '[name].js',
+    filename: '[name].min.js',
     path: path.join(__dirname, 'dist'),
     library: 'setImmediateInterval',
     libraryTarget: 'umd',
